1ea0f5129016ebdbd9353d645c1283f5a1e774e7
LUISDACA <luis.davidca@campusucc.edu.co>
2025-05-30 01:20:01 -0500

    Fix: permisos de ejecución para startup.sh

--- SaboresCaseros/startup.sh
+++ SaboresCaseros/startup.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --noinput
 echo "✅ Mostrando ubicación y paquetes instalados"
 which python
 python -m pip list
+
+echo "🚀 Iniciando servidor Gunicorn..."
+gunicorn SaboresCaseros.wsgi:application --bind 0.0.0.0:8000
