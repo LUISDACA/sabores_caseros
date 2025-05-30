09fce3c1e157efed1673577b4d627d7dfa572ffb
LUISDACA <luis.davidca@campusucc.edu.co>
2025-05-30 01:13:18 -0500

    21

--- SaboresCaseros/startup.sh
+++ SaboresCaseros/startup.sh
@@ -6,3 +6,7 @@ python manage.py migrate --noinput
 
 echo "🗂️ Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput
+
+echo "✅ Mostrando ubicación y paquetes instalados"
+which python
+python -m pip list
