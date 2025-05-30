3c5fcee69d9ef1e76c4974b2b434e65632cc41b8
LUISDACA <luis.davidca@campusucc.edu.co>
2025-05-30 01:45:48 -0500

    23

--- SaboresCaseros/startup.sh
+++ SaboresCaseros/startup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-set -e
-
-echo "📦 Aplicando migraciones..."
+echo "✅ Aplicando migraciones..."
 python manage.py migrate --noinput
 
-echo "🗂️ Recolectando archivos estáticos..."
+echo "📦 Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput
 
-echo "✅ Mostrando ubicación y paquetes instalados"
-which python
-python -m pip list
+echo "🚀 Iniciando servidor Gunicorn..."
+gunicorn SaboresCaseros.wsgi:application --bind 0.0.0.0:8000
